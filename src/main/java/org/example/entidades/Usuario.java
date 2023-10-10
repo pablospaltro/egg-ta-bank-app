aee8661f7ff1dcb0b25ad4503645d23fe7cd3735
pablospaltro5@gmail.com <pablospaltro5@gmail.com>
2023-10-10 19:45:40 -0300

    Agrego proyecto con lombok

---
+++ src/main/java/org/example/entidades/Usuario.java
@@ -0,0 +1,15 @@
+package org.example.entidades;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    String usuarioNombre;
+    String usuarioPassword;
+    int usuarioFondos;
+
+
+}
